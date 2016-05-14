c1319c6af0ea44a6ffa290c1d6de83b253514b68
Drew Keller <drew@eightycolumns.com>
2016-05-14 19:02:29 -0400

    Test one-minute row at minute 0

---
+++ spec/BerlinClockSpec.js
@@ -0,0 +1,16 @@
+var BerlinClock = require('../lib/BerlinClock');
+
+describe('Berlin Clock', function () {
+  var berlinClock;
+
+  beforeEach(function () {
+    berlinClock = new BerlinClock();
+  });
+
+  describe('one-minute row', function () {
+    it('is OOOO at minute 0', function () {
+      berlinClock.setTime(0, 0, 0);
+      expect(berlinClock.oneMinuteRow()).toBe('OOOO');
+    });
+  });
+});
